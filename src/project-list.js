c9c0c0c85e579ae1b58998568a9e8e716e8e22c0
Lipatant <virgile.berrier@gmail.com>
2025-04-24 13:40:03 +0200

    feat: adding project icons

--- src/project-list.js
+++ src/project-list.js
@@ -46,56 +46,56 @@ const languageDataList = {
 
 const projectList = document.getElementById("project-list")
 
-const projectDataList = [
-    {
+const projectDataList = {
+    "casual_tryhard": {
         "description": "A minigame-based arcade game",
         "languages": ["godot"],
         "name": "Casual Tryhard",
         "tags": ["game", "game jam"],
     },
-    {
+    "checkfate": {
         "description": "A chess-inspired surviving strategy game",
         "languages": ["cpp"],
         "name": "Checkfate",
         "tags": ["game", "game jam"],
     },
-    {
+    "dissonance": {
         "description": "A puzzle game about controlling two characters",
         "languages": ["rgss3"],
         "name": "Dissonance",
         "tags": ["game", "game jam"],
     },
-    {
+    "kitchenware_shop": {
         "description": "An absurd visual novel about meeting kitchen ustensils",
         "languages": ["godot"],
         "name": "Kitchenware Shop",
         "tags": ["browser", "game", "game jam"],
     },
-    {
+    "lipatants_artefact": {
         "description": "A Minecraft data pack adding new items, completed by a web-based wiki",
         "languages": ["css", "html", "js", "mcfunction", "powershell", "python"],
         "name": "Lipatant's Arfefact",
         "tags": ["mod", "web"],
     },
-    {
+    "mordern_wizard": {
         "description": "A first-person shooter about surviving waves of zombies and extracing artefacts",
         "languages": ["unreal engine"],
         "name": "Modern Wizard",
         "tags": ["epitech", "game"],
     },
-    {
+    "protfolio": {
         "description": "This portfolio, used for displaying my work",
         "languages": ["css", "html", "js"],
         "name": "Portfolio",
         "tags": ["web"],
     },
-    {
+    "raytracer": {
         "description": "A raytracing-based rendering program using no GPU",
         "languages": ["cpp"],
         "name": "Raytracer",
         "tags": ["epitech"],
     },
-]
+}
 
 function createLanguageTag(language) {
     let element = document.createElement("a")
@@ -118,7 +118,17 @@ function createLanguageTag(language) {
     return element
 }
 
-function createProjectDataDescription(projectData) {
+function createProjectDataContent(projectID, projectData) {
+    let element = document.createElement("div")
+    let elementDescription = document.createElement("div")
+    elementDescription.appendChild(createProjectDataDescription(projectID, projectData))
+    elementDescription.appendChild(createProjectDataLanguages(projectID, projectData))
+    element.appendChild(createProjectDataImage(projectID, projectData))
+    element.appendChild(elementDescription)
+    return element
+}
+
+function createProjectDataDescription(projectID, projectData) {
     let element = document.createElement("a")
     element.classList.add("description")
     if ("description" in projectData) {
@@ -127,7 +137,14 @@ function createProjectDataDescription(projectData) {
     return element;
 }
 
-function createProjectDataLanguages(projectData) {
+function createProjectDataImage(projectID, projectData) {
+    let element = document.createElement("img")
+    element.setAttribute("alt", "");
+    element.setAttribute("src", `img/project/${projectID}/icon.png`);
+    return element
+}
+
+function createProjectDataLanguages(projectID, projectData) {
     let element = document.createElement("div")
     element.classList.add("languages")
     if ("languages" in projectData) {
@@ -138,25 +155,28 @@ function createProjectDataLanguages(projectData) {
     return element;
 }
 
-function createProjectDataTitle(projectData) {
+function createProjectDataTitle(projectID, projectData) {
     let element = document.createElement("div")
     let elementName = document.createElement("a")
-    elementName.innerHTML = projectData["name"]
+    if ("name" in projectData) {
+        elementName.innerHTML = projectData["name"]
+    } else {
+        elementName.innerHTML = projectID
+    }
     element.appendChild(elementName)
     return element;
 }
 
-function createProjectData(projectData) {
+function createProjectData(projectID, projectData) {
     let element = document.createElement("div")
-    element.appendChild(createProjectDataTitle(projectData))
-    element.appendChild(createProjectDataDescription(projectData))
-    element.appendChild(createProjectDataLanguages(projectData))
+    element.appendChild(createProjectDataTitle(projectID, projectData))
+    element.appendChild(createProjectDataContent(projectID, projectData))
     element.classList.add("project")
     return element;
 }
 
 if (projectList) {
-    for (const projectData of projectDataList) {
-        projectList.appendChild(createProjectData(projectData))
+    for (const [projectID, projectData] of Object.entries(projectDataList)) {
+        projectList.appendChild(createProjectData(projectID, projectData))
     }
 }
